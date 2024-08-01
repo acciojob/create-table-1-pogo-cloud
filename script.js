26ba5369424497a7a4110c35a9128176e16395f0
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-08-01 18:48:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function insert_Row() {
-    //Write your code here
-  
-  
-}
+let table_top = document.querySelector("#sampleTable");
+
+        function insert_Row() {
+            let tableRow = document.createElement("tr");
+            let tcolOne = document.createElement("td");
+            let tcolTwo = document.createElement("td");
+            tcolOne.innerHTML = "New Cell1"
+            tcolTwo.innerHTML = "New Cell2";
+            tableRow.appendChild(tcolOne);
+            tableRow.appendChild(tcolTwo);
+            table_top.prepend(tableRow);
+        }
